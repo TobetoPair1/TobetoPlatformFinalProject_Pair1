1dab8da08ed7b1187d16a84e4de85f10b62db0a8
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2024-01-13 16:40:15 +0300

    IUserCourse deleted.

--- DataAccess/Abstracts/IUserCourse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.DataAccess.Repositories;
-using Entities.Concretes;
-
-namespace DataAccess.Abstracts
-{
-	public interface IUserCourse: IAsyncRepository<Homework, Guid>, IRepository<Homework, Guid>
-	{
-    }
-}
