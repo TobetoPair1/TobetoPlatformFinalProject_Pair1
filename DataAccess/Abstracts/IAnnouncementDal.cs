187105a1a5e4b4ca588acd9cf03f6d52103a1438
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2024-01-13 16:34:38 +0300

    DataAccess Abstracts Edited

--- DataAccess/Abstracts/IAnnouncementDal.cs
+++ DataAccess/Abstracts/IAnnouncementDal.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.DataAccess.Repositories;
+using Entities.Concretes;
 
 namespace DataAccess.Abstracts
 {
-    public interface IAnnouncementDal: IAsyncRepository<Announcement, Guid>, IRepository<Announcement, Guid>
+	public interface IAnnouncementDal: IAsyncRepository<Announcement, Guid>, IRepository<Announcement, Guid>
     {
     }
 }
