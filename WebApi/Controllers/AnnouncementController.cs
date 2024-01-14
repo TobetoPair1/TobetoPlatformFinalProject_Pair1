5af76a0b61a2257bc1752cf4f23a8ec457a9a5e3
Atilla <atillaagungorr@gmail.com>
2024-01-14 14:32:56 +0300

    Announcement controller added

--- WebApi/Controllers/AnnouncementController.cs
+++ WebApi/Controllers/AnnouncementController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstracts;
+using Business.Dtos.Requests.Announcement;
+using Business.Dtos.Requests.Certificate;
+using Core.DataAccess.Paging;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
@@ -7,5 +11,37 @@ namespace WebApi.Controllers
     [ApiController]
     public class AnnouncementController : ControllerBase
     {
+        private readonly IAnnouncementService _announcementService;
+        public AnnouncementController(IAnnouncementService announcementService)
+        {
+            _announcementService = announcementService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateAnnouncementRequest announcementRequest)
+        {
+            var result = await _announcementService.AddAsync(announcementRequest);
+            return Ok(result);
+        }
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get([FromQuery] GetAnnouncementRequest getAnnouncementRequest)
+        {
+            var result = await _announcementService.GetByIdAsync(getAnnouncementRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll([FromQuery] PageRequest pageRequest)
+        {
+            var result = await _announcementService.GetListAsync(pageRequest);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteAnnouncementRequest deleteAnnouncementRequest)
+        {
+            var result = await _announcementService.DeleteAsync(deleteAnnouncementRequest);
+            return Ok(result);
+        }
     }
 }
