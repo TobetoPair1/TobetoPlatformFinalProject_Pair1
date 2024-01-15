af714dcfe282a99215ad15fc4727f22ba7af39a1
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2024-01-15 18:11:55 +0300

    AutofacBusinessModule

---
+++ Business/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using System.Reflection;
+
+namespace Business
+{
+	public class AutofacBusinessModule: Autofac.Module
+    {
+		protected override void Load(ContainerBuilder builder)
+		{
+			var assembly = Assembly.GetExecutingAssembly();			
+
+			builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+				.EnableInterfaceInterceptors(new ProxyGenerationOptions()
+				{
+					Selector = new AspectInterceptorSelector()
+				}).SingleInstance();
+		}
+	}
+}
