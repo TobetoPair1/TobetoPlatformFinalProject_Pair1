bf1625436938cb71d8d3535d4e2d494d1fa335cc
bbiircan <bircanbircan1963@gmail.com>
2024-01-16 23:06:52 +0300

    Question controller added

---
+++ WebApi/Controllers/QuestionsController.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests.Question;
+using Core.DataAccess.Paging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        IQuestionService _questionService;
+
+        public QuestionsController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateQuestionRequest createQuestionRequest)
+        {
+            var result = await _questionService.AddAsync(createQuestionRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            var result = await _questionService.GetListAsync(pageRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get([FromQuery] GetQuestionRequest getQuestionRequest)
+        {
+            var result = await _questionService.GetByIdAsync(getQuestionRequest);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteQuestionRequest deleteQuestionRequest)
+        {
+            var result = await _questionService.DeleteAsync(deleteQuestionRequest);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateQuestionRequest updateQuestionRequest)
+        {
+            var result = await _questionService.UpdateAsync(updateQuestionRequest);
+            return Ok(result);
+        }
+    }
+}
