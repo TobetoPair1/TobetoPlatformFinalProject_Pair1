2256fca564ebf998ca803688c12e4258fd3e76c7
bbiircan <bircanbircan1963@gmail.com>
2024-01-16 17:23:37 +0300

    UserExamsController, UserSurveysController Added

---
+++ WebApi/Controllers/UserExamsController.cs
@@ -0,0 +1,47 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests.UserExam;
+using Core.DataAccess.Paging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserExamsController : ControllerBase
+    {
+        IUserExamService _userExamService;
+
+        public UserExamsController(IUserExamService userExamService)
+        {
+            _userExamService = userExamService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateUserExamRequest createUserExamRequest)
+        {
+            var result = await _userExamService.AddAsync(createUserExamRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            var result = await _userExamService.GetListAsync(pageRequest);
+            return Ok(result);  
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get([FromQuery] GetUserExamRequest getUserExamRequest)
+        {
+            var result = await _userExamService.GetByIdAsync(getUserExamRequest);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteUserExamRequest deleteUserExamRequest)
+        {
+            var result = await _userExamService.DeleteAsync(deleteUserExamRequest);
+            return Ok(result);
+        }
+    }
+}
---
+++ WebApi/Controllers/UserSurveysController.cs
@@ -0,0 +1,47 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests.UserSurvey;
+using Core.DataAccess.Paging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserSurveysController : ControllerBase
+    {
+        IUserSurveyService _userSurveyService;
+
+        public UserSurveysController(IUserSurveyService userSurveyService)
+        {
+            _userSurveyService = userSurveyService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateUserSurveyRequest createUserSurveyRequest)
+        {
+            var result = await _userSurveyService.AddAsync(createUserSurveyRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            var result = await _userSurveyService.GetListAsync(pageRequest);
+            return Ok(result);  
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get([FromQuery] GetUserSurveyRequest getUserSurveyRequest)
+        {
+            var result = await _userSurveyService.GetByIdAsync(getUserSurveyRequest);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteUserSurveyRequest deleteUserSurveyRequest)
+        {
+            var result = await _userSurveyService.DeleteAsync(deleteUserSurveyRequest);
+            return Ok(result);  
+        }
+    }
+}
