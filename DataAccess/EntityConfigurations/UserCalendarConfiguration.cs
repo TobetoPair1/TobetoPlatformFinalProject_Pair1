bbe152cabce6bc21ff6555b609708f2a76ff8750
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2024-01-23 13:02:18 +0300

    edited last commit

--- DataAccess/EntityConfigurations/UserCalendarConfiguration.cs
+++ DataAccess/EntityConfigurations/UserCalendarConfiguration.cs
@@ -8,20 +8,13 @@ namespace DataAccess.EntityConfigurations
 	{
 		public void Configure(EntityTypeBuilder<UserCalendar> builder)
 		{
-			builder.ToTable("Categories").HasKey(c => c.Id);
+			builder.Ignore(u => u.Id);
+			builder.ToTable("UserCalendars").HasKey(u => new { u.UserId, u.CalenderId });
 
-			builder.Property(c => c.Id).HasColumnName("Id").IsRequired();
-			builder.Property(c => c.Name).HasColumnName("Name").IsRequired();
+			builder.Property(u => u.UserId).HasColumnName("UserId").IsRequired();
+			builder.Property(u => u.CalenderId).HasColumnName("CalenderId").IsRequired();
 
-			builder.HasIndex(u => u.Name, "UK_Categories_Name").IsUnique();
-
-			builder.HasQueryFilter(c => !c.DeletedDate.HasValue);
-
-			builder.HasMany(c => c.Courses).WithOne(c => c.Category).HasForeignKey(c => c.CategoryId);
-			builder.HasMany(c => c.LiveContents).WithOne(lc => lc.Category).HasForeignKey(lc => lc.CategoryId);
-			builder.HasMany(c => c.AsyncContents).WithOne(ac => ac.Category).HasForeignKey(ac => ac.CategoryId);
-			builder.HasMany(c => c.Assignments).WithOne(a => a.Category).HasForeignKey(a => a.CategoryId);
+			builder.HasQueryFilter(u => !u.DeletedDate.HasValue);
 		}
 	}
-
 }
