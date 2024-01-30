93c511b1dcd8f811d44f9f43137940c7b1403b4d
Sabır Akbana <s.akbana9o@gmail.com>
2024-01-30 19:58:54 +0300

    CourseLiveContent, LiveContent, LiveContentsController Updated

--- Business/Dtos/Requests/LiveContent/CreateLiveContentRequest.cs
+++ Business/Dtos/Requests/LiveContent/CreateLiveContentRequest.cs
@@ -3,6 +3,7 @@
 	public class CreateLiveContentRequest
     {
         public Guid LikeId { get; set; }
+        public Guid CategoryId { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
         public bool IsCompleted { get; set; }
