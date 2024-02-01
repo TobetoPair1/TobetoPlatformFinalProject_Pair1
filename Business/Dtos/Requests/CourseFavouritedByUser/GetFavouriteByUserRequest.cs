d3271d19845ebd80abc94d8022bf59698b07f0fb
Sabır Akbana <s.akbana9o@gmail.com>
2024-02-01 17:05:16 +0300

    Favourite Request Added

---
+++ Business/Dtos/Requests/CourseFavouritedByUser/GetFavouriteByUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Business.Dtos.Requests.CourseFavouritedByUser
+{
+    public class GetFavouriteByUserRequest
+    {
+        public Guid UserId { get; set; }
+        public Guid CourseId { get; set; }
+    }
+}
