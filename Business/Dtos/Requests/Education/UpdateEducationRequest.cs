38b3f002c5abdb13ccb8b76ad13550617fced31b
Sabır Akbana <s.akbana9o@gmail.com>
2024-02-01 19:26:14 +0300

    requests updated

--- Business/Dtos/Requests/Education/UpdateEducationRequest.cs
+++ Business/Dtos/Requests/Education/UpdateEducationRequest.cs
@@ -3,11 +3,11 @@
 	public class UpdateEducationRequest
     {
         public Guid UserId { get; set; }
-        public string? EducationLevel { get; set; }
-        public string? University { get; set; }
-        public string? Department { get; set; }
-        public int? StartofDate { get; set; }
-        public int? GraduationYear { get; set; }
-        public bool? IsContinued { get; set; }
+        public string EducationLevel { get; set; }
+        public string University { get; set; }
+        public string Department { get; set; }
+        public int StartofDate { get; set; }
+        public int GraduationYear { get; set; }
+        public bool IsContinued { get; set; }
     }
 }
