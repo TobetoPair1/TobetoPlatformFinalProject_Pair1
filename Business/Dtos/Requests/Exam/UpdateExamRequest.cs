e7acb91389ab74b4c8cb293c0af20c5d36d06b9f
bbiircan <bircanbircan1963@gmail.com>
2024-02-01 19:27:17 +0300

    Exam, Experience, Favourite, File updated

--- Business/Dtos/Requests/Exam/UpdateExamRequest.cs
+++ Business/Dtos/Requests/Exam/UpdateExamRequest.cs
@@ -3,17 +3,17 @@
 	public class UpdateExamRequest
     {
 		public Guid Id { get; set; }
-		public string? Title { get; set; }
-        public string? Description { get; set; }
-        public DateTime? StartOfDate { get; set; }
-        public DateTime? EndOfTime { get; set; }
-        public ulong? Time { get; set; }
-        public bool? IsCompleted { get; set; }
-        public int? QuestionCount { get; set; }
-        public int? Score { get; set; }
+		public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartOfDate { get; set; }
+        public DateTime EndOfTime { get; set; }
+        public ulong Time { get; set; }
+        public bool IsCompleted { get; set; }
+        public int QuestionCount { get; set; }
+        public int Score { get; set; }
         public string Type { get; set; }
-        public int? TrueCount { get; set; }
-        public int? FalseCount { get; set; }
-        public int? EmptyCount { get; set; }
+        public int TrueCount { get; set; }
+        public int FalseCount { get; set; }
+        public int EmptyCount { get; set; }
     }
 }
