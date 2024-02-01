355233b39587b42ddee692253e7e96cead216cf3
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2024-02-01 17:22:34 +0300

    CourseLikedByUserProfile

---
+++ Business/Profiles/CourseLikedByUserProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Business.Dtos.Requests.CourseLikedByUser;
+using Business.Dtos.Responses.CourseLikedByUser;
+using Core.DataAccess.Paging;
+using Entities.Concretes.CrossTables;
+
+namespace Business.Profiles
+{
+	public class CourseLikedByUserProfile:Profile
+	{
+        public CourseLikedByUserProfile()
+        {
+			CreateMap<CourseLikedByUser, CreateCourseLikedByUserRequest>().ReverseMap();
+			CreateMap<CourseLikedByUser, CreatedCourseLikedByUserResponse>().ReverseMap();			
+
+			CreateMap<CourseLikedByUser, DeleteCourseLikedByUserRequest>().ReverseMap();
+			CreateMap<CourseLikedByUser, DeletedCourseLikedByUserResponse>().ReverseMap();
+
+			CreateMap<IPaginate<CourseLikedByUser>, Paginate<GetListCourseLikedByUserResponse>>().ReverseMap();
+			CreateMap<CourseLikedByUser, GetListCourseLikedByUserResponse>().ReverseMap();
+
+			CreateMap<CourseLikedByUser, GetCourseLikedByUserRequest>().ReverseMap();
+			CreateMap<CourseLikedByUser, GetCourseLikedByUserResponse>().ReverseMap();
+		}
+    }
+}
