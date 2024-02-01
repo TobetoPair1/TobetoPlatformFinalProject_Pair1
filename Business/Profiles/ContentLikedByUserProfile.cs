89df74e57b398d073f0eacf3fdfe0ffc64414e7d
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2024-02-01 17:07:46 +0300

    ContentLikedByUserProfile

---
+++ Business/Profiles/ContentLikedByUserProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Business.Dtos.Requests.ContentLikedByUser;
+using Business.Dtos.Responses.ContentLikedByUser;
+using Core.DataAccess.Paging;
+using Entities.Concretes.CrossTables;
+
+namespace Business.Profiles
+{
+	public class ContentLikedByUserProfile:Profile
+	{
+		public ContentLikedByUserProfile()
+		{
+			CreateMap<ContentLikedByUser, CreateContentLikedByUserRequest>().ReverseMap();
+			CreateMap<ContentLikedByUser, CreatedContentLikedByUserResponse>().ReverseMap();
+
+			CreateMap<ContentLikedByUser, DeleteContentLikedByUserRequest>().ReverseMap();
+			CreateMap<ContentLikedByUser, DeletedContentLikedByUserResponse>().ReverseMap();
+
+			CreateMap<IPaginate<ContentLikedByUser>, Paginate<GetListContentLikedByUserResponse>>().ReverseMap();
+			CreateMap<ContentLikedByUser, GetListContentLikedByUserResponse>().ReverseMap();
+
+			CreateMap<ContentLikedByUser, GetContentLikedByUserRequest>().ReverseMap();
+			CreateMap<ContentLikedByUser, GetContentLikedByUserReponse>().ReverseMap();
+		}
+	}
+}
