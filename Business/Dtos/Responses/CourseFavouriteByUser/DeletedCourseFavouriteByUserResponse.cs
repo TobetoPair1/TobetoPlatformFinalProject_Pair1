87a9267abe86a59c4f11160d51fc43b4b64140ae
bbiircan <bircanbircan1963@gmail.com>
2024-02-01 17:11:04 +0300

    CourseFavouriteByUser

---
+++ Business/Dtos/Responses/CourseFavouriteByUser/DeletedCourseFavouriteByUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Business.Dtos.Responses.CourseFavouriteByUser
+{
+    public class DeletedCourseFavouriteByUserResponse
+    {
+        public Guid UserId { get; set; }
+        public Guid CourseId { get; set; }
+    }
+}
---
+++ Business/Dtos/Responses/CourseFavouriteByUser/GetListCourseFavouriteByUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Business.Dtos.Responses.CourseFavouriteByUser
+{
+    public class GetListCourseFavouriteByUserResponse
+    {
+        public Guid UserId { get; set; }
+        public Guid CourseId { get; set; }
+    }
+}
