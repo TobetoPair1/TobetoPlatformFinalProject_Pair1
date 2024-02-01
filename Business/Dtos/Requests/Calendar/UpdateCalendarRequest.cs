7f35c404f5ff81c635e448a729e0f0bdcc50d8c4
Mehmet Albayrak <albayrakmehmetdeu@gmail.com>
2024-02-01 19:27:43 +0300

    dtos are updated

--- Business/Dtos/Requests/Calendar/UpdateCalendarRequest.cs
+++ Business/Dtos/Requests/Calendar/UpdateCalendarRequest.cs
@@ -5,7 +5,7 @@ public class UpdateCalendarRequest
     public Guid Id { get; set; }
     public Guid? InstructorId { get; set; }
     public Guid? CourseId { get; set; }
-    public bool? IsCompleted { get; set; }
-    public bool? IsPurchased { get; set; }
-    public DateTime? Date {  get; set; }
+    public bool IsCompleted { get; set; }
+    public bool IsPurchased { get; set; }
+    public DateTime Date {  get; set; }
 }
--- Business/Dtos/Responses/Calender/UpdatedCalendarResponse.cs
+++ Business/Dtos/Responses/Calender/UpdatedCalendarResponse.cs
@@ -3,9 +3,9 @@
 public class UpdatedCalendarResponse
 {
     public Guid Id { get; set; }
-    public Guid? InstructorId { get; set; }
-    public Guid? CourseId { get; set; }
-    public bool? IsCompleted { get; set; }
-    public bool? IsPurchased { get; set; }
-    public DateTime? Date { get; set; }
+    public Guid InstructorId { get; set; }
+    public Guid CourseId { get; set; }
+    public bool IsCompleted { get; set; }
+    public bool IsPurchased { get; set; }
+    public DateTime Date { get; set; }
 }
