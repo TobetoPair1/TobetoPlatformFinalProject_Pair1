dc54ac996794a7adfdf7bb81edfba419c1f51fd0
bbiircan <bircanbircan1963@gmail.com>
2024-02-01 16:56:49 +0300

    CourseLikedByUser

---
+++ Business/Dtos/Responses/CourseLikedByUser/GetCourseLikedByUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Business.Dtos.Responses.CourseLikedByUser
+{
+    public class GetCourseLikedByUserResponse
+    {
+        public Guid UserId { get; set; }
+        public Guid ContentId { get; set; }
+    }
+}
