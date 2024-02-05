357bd3e3c722ba3e4b33d682d731d879b2ab8634
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2024-02-05 16:40:47 +0300

    UserManager updated getById

--- Business/Abstracts/IUserService.cs
+++ Business/Abstracts/IUserService.cs
@@ -12,7 +12,7 @@ public interface IUserService
     Task<DeletedUserResponse> DeleteByIdAsync(Guid id);
     Task<DeletedUserResponse> DeleteByMailAsync(string email);
     Task<UpdatedUserResponse> UpdateAsync(UpdateUserRequest updateUserRequest);
-    Task<GetUserResponse> GetAsync(GetUserRequest getUserRequest);
+    Task<GetUserResponse> GetByIdAsync(Guid id);
     Task<User> GetByMailAsync(string mail,bool withDeleted);
     Task<GetByMailUserResponse> GetByMailUserAsync(string mail);
     List<IOperationClaim> GetClaims(IUser user);
