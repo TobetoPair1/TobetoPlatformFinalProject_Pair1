4147a42e56224ac29a86592d7568af354d051ccf
bbiircan <bircanbircan1963@gmail.com>
2024-02-05 17:03:31 +0300

    ForeignLanguageBusinessRules updated

--- Business/Rules/ForeignLanguageRules.cs
+++ Business/Rules/ForeignLanguageRules.cs
@@ -1,4 +1,8 @@
+using Business.Abstracts;
+using Business.Dtos.Responses.User;
 using Core.Business.Rules;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Core.Utilities.Messages;
 using DataAccess.Abstracts;
 using Entities.Concretes;
 
@@ -7,8 +11,19 @@ namespace Business.Rules;
 public class ForeignLanguageBusinessRules : BaseBusinessRules<ForeignLanguage>
 {
     IForeignLanguageDal _foreignLanguageDal;
-    public ForeignLanguageBusinessRules(IForeignLanguageDal foreignLanguageDal) : base(foreignLanguageDal)
+    IUserService _userService;
+    public ForeignLanguageBusinessRules(IForeignLanguageDal foreignLanguageDal, IUserService userService) : base(foreignLanguageDal)
     {
         _foreignLanguageDal = foreignLanguageDal;
+        _userService = userService;
     }
+    public async Task CheckUserIfExists(Guid userId)
+    {
+        GetUserResponse user = await _userService.GetByIdAsync(userId);
+        if (user == null)
+        {
+            throw new BusinessException(BusinessCoreMessages.CannotFindEntityError, BusinessCoreTitles.CannotFindError);
+        }
+    }
+
 }
