e4796a9374999384f0a2a81e5667624d54f1aba6
Sabır Akbana <s.akbana9o@gmail.com>
2024-02-07 17:18:55 +0300

    Announcement

--- Business/Dtos/Responses/Announcement/GetListAnnouncementResponse.cs
+++ Business/Dtos/Responses/Announcement/GetListAnnouncementResponse.cs
@@ -5,5 +5,6 @@
         public Guid Id { get; set; }
         public string Header { get; set; }
         public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
