9ef63c702bd3059826d22a8aea93891c8b2fa305
Sabır Akbana <s.akbana9o@gmail.com>
2024-02-13 17:28:39 +0300

    Survey

--- Business/Dtos/Responses/Survey/GetListSurveyResponse.cs
+++ Business/Dtos/Responses/Survey/GetListSurveyResponse.cs
@@ -6,5 +6,6 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public string FormUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
