0fd659e4e559634b59538613dc86718f319063a8
Atilla <atillaagungorr@gmail.com>
2024-02-16 15:21:13 +0300

    SecuredOperations added to project

--- Business/Concretes/EducationManager.cs
+++ Business/Concretes/EducationManager.cs
@@ -4,6 +4,7 @@ using Business.Dtos.Requests.Course;
 using Business.Dtos.Requests.Education;
 using Business.Dtos.Responses.Education;
 using Business.Rules;
+using Core.Aspects.Autofac.SecuredOperation;
 using Core.DataAccess.Paging;
 using DataAccess.Abstracts;
 using Entities.Concretes;
@@ -21,14 +22,15 @@ public class EducationManager : IEducationService
 		_educationDal = educationDal;
 		_educationBusinessRules = educationBusinessRules;
 	}
-	public async Task<CreatedEducationResponse> AddAsync(CreateEducationRequest createEducationRequest)
+    [SecuredOperation("admin")]
+    public async Task<CreatedEducationResponse> AddAsync(CreateEducationRequest createEducationRequest)
     {
         Education education = _mapper.Map<Education>(createEducationRequest);
         var createdEducation = await _educationDal.AddAsync(education);
         CreatedEducationResponse result = _mapper.Map<CreatedEducationResponse>(createdEducation);
         return result;
     }
-
+    [SecuredOperation("admin")]
     public async Task<DeletedEducationResponse> DeleteAsync(DeleteEducationRequest deleteEducationRequest)
     {
         Education education = await _educationBusinessRules.CheckIfExistsById(deleteEducationRequest.Id);
@@ -42,7 +44,7 @@ public class EducationManager : IEducationService
         var result = await _educationDal.GetAsync(e => e.Id == getEducationRequest.Id);
         return _mapper.Map<GetEducationResponse>(result);
     }
-
+    [SecuredOperation("admin")]
     public async Task<IPaginate<GetListEducationResponse>> GetListAsync(PageRequest pageRequest)
     {
         var result = await _educationDal.GetListAsync(index: pageRequest.PageIndex, size: pageRequest.PageSize);
@@ -54,7 +56,7 @@ public class EducationManager : IEducationService
         var result = await _educationDal.GetListAsync(e => e.UserId == userId, index: pageRequest.PageIndex, size: pageRequest.PageSize);
         return _mapper.Map<Paginate<GetListEducationResponse>>(result);
     }
-
+    [SecuredOperation("admin")]
     public async Task<UpdatedEducationResponse> UpdateAsync(UpdateEducationRequest updateEducationRequest)
     {
         await _educationBusinessRules.CheckUserIfExists(updateEducationRequest.UserId);
--- Business/Concretes/ExperienceManager.cs
+++ Business/Concretes/ExperienceManager.cs
@@ -3,6 +3,7 @@ using Business.Abstracts;
 using Business.Dtos.Requests.Experience;
 using Business.Dtos.Responses.Experience;
 using Business.Rules;
+using Core.Aspects.Autofac.SecuredOperation;
 using Core.DataAccess.Paging;
 using DataAccess.Abstracts;
 using Entities.Concretes;
@@ -23,7 +24,7 @@ public class ExperienceManager : IExperienceService
         _mapper = mapper;
         _experienceBusinessRules = experienceBusinessRules;
     }
-
+    [SecuredOperation("admin")]
     public async Task<CreatedExperienceResponse> AddAsync(CreateExperienceRequest createExperienceRequest)
     {
         Experience experience = _mapper.Map<Experience>(createExperienceRequest);
@@ -31,7 +32,7 @@ public class ExperienceManager : IExperienceService
         CreatedExperienceResponse result = _mapper.Map<CreatedExperienceResponse>(createdExperience);
         return result;
     }
-
+    [SecuredOperation("admin")]
     public async Task<DeletedExperienceResponse> DeleteAsync(DeleteExperienceRequest deleteExperienceRequest)
     {
         Experience experience = await _experienceBusinessRules.CheckIfExistsById(deleteExperienceRequest.Id);
@@ -46,7 +47,7 @@ public class ExperienceManager : IExperienceService
         var result = await _experienceDal.GetAsync(i => i.Id == getExperienceRequest.Id);
         return _mapper.Map<GetExperienceResponse>(result);
     }
-
+    [SecuredOperation("admin")]
     public async Task<IPaginate<GetListExperienceResponse>> GetListAsync(PageRequest pageRequest)
     {
         var result = await _experienceDal.GetListAsync(index: pageRequest.PageIndex, size: pageRequest.PageSize);
@@ -58,7 +59,7 @@ public class ExperienceManager : IExperienceService
         var result = await _experienceDal.GetListAsync(e => e.UserId == userId, index: pageRequest.PageIndex, size: pageRequest.PageSize);
         return _mapper.Map<Paginate<GetListExperienceResponse>>(result);
     }
-    
+    [SecuredOperation("admin")]
     public async Task<UpdatedExperienceResponse> UpdateAsync(UpdateExperienceRequest updateExperienceRequest)
     {
         await _experienceBusinessRules.CheckUserIfExists(updateExperienceRequest.Id);
